a413a88ad7c4b53f19c1a17fae09e8afae7db0b8
TeaHaven04 <juancarlos.nicdao@my.jru.edu>
2025-02-03 17:58:33 +0800

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $servername = "	sql101.infinityfree.com"; // The database host from InfinityFree
 $username = "if0_38233040"; // Your database username
 $password = "ddZHoh7v34Lj"; // Your database password
-$dbname = "if0_38233040_teahaven"; // Your database name
+$dbname = "teahaven"; // Your database name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
